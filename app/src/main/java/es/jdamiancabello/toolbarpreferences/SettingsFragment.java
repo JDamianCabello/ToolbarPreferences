421404fec263312ee9d808dc176fe7c81a8a406b
Damián Cabello <jdamiancabello@gmail.com>
2019-12-11 10:10:37 +0100

    No furula bien

--- app/src/main/java/es/jdamiancabello/toolbarpreferences/SettingsFragment.java
+++ app/src/main/java/es/jdamiancabello/toolbarpreferences/SettingsFragment.java
@@ -1,12 +1,38 @@
 package es.jdamiancabello.toolbarpreferences;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.settings_preferences);
+        //Se quere recoger el evento cuando se modifique el String
+        onSharedPreferenceChanged(PreferenceManager.getDefaultSharedPreferences(getActivity()),getString(R.string.key_ringtone_notification));
+
+        onSharedPreferenceChanged(PreferenceManager.getDefaultSharedPreferences(getActivity()),getString(R.string.key_psw));
+
     }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+
+        if(key.equals(getString(R.string.key_ringtone_notification))){
+            //Downcasting
+            ListPreference listPreference = (ListPreference)preference;
+            int prefIndex = listPreference.findIndexOfValue(sharedPreferences.getString(key,null));
+
+            if(prefIndex >= 0)
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            else
+                preference.setSummary(sharedPreferences.getString(key,null));
+        }
+    }
+
 }
